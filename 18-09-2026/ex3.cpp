65ea85f75753b23c59bcd31e5a00aea424c3d74a
Álvaro Pereira <alvarozord@gmail.com>
2026-09-18 21:39:09 -0300

    feat: merge sort e matrizes em ação

---
+++ 18-09-2026/ex3.cpp
@@ -0,0 +1,10 @@
+// 3 - Declare duas matrizes 3x3, some-as elemento por elemento armazenando o resultado em uma terceira matriz. Crie uma função separada para imprimir qualquer matriz 3x3.
+
+#include <iostream> 
+using namespace std;
+
+int main(void){
+
+
+    return 0;
+}
